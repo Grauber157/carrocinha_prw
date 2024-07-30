798ec7744791d9eb400ad4b217b64d4d16d9794e
Grauber157 <faria.dias@aluno.ifsp.edu.br>
2024-07-30 12:18:38 -0300

    Terminar

---
+++ pages_pessoa/listarPessoa.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button><a href="../index.html">Voltar ao Menu</a></button><br>
+    <?php
+        include("../include/conexao.php");
+
+        $sql = "SELECT cli.id, cli.nome nomecliente, cli.email, cli.ativo, cid.nome nomecidade, cid.estado FROM cliente cli LEFT JOIN cidade cid on cid.id = cli.id_cidade";
+        $result = mysqli_query($con, $sql);
+        //$row = mysqli_fetch_array($result);
+
+    ?>
+    <h1>Consulta de Pessoas</h1>
+    <button><a href="cadastrarPessoa.php">Cadastrar Nova Pessoa</a></button>
+    
+    <table align="center" border="1" width="500">
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ativo</th>
+            <th>Cidade</th>
+            <th>Estado</th>
+            <th>Alterar</th>
+            <th>Deletar</th>
+        </tr>
+    <?php
+        while($row = mysqli_fetch_array($result))
+        {
+            $ativo = $row['ativo'] == 1 ? "Sim": "Não";
+            echo "<tr>";
+            echo "<td>".$row['id']."</td>";
+            echo "<td>".$row['nomecliente']."</td>";
+            echo "<td>".$row['email']."</td>";
+            echo "<td>".$ativo."</td>";
+            echo "<td>".$row['nomecidade']."</td>";
+            echo "<td>".$row['estado']."</td>";
+            echo "<td><a href='AlterarCliente.php?id=".$row['id']."'>Alterar</a></td>";
+            echo "<td><a href='DeletarCliente.php?id=".$row['id']."'>Deletar</a></td>";
+            echo "<tr>";
+        }
+    ?>
+    </table>
+</body>
+</html>
